ee751699d35bbda6fef65b1cd79916caa5d1a987
HtetPhyoeMaung <htetphyoemaung@edusn.co.uk>
2024-10-20 13:24:50 +0630

    fix error

--- src/main/java/com/edusn/Digizenger/Demo/config/WebSocketConfig.java
+++ src/main/java/com/edusn/Digizenger/Demo/config/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/user");
-        config.setApplicationDestinationPrefixes("/app");4
+        config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
 
