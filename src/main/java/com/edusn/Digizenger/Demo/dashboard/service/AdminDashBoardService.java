ed2f0b9ee2166f051762488603fd2ba5e964405b
KyawChitMoe9 <kyawchitmoe@edusn.co.uk>
2024-11-05 15:42:13 +0630

    getSuspendedUses

--- src/main/java/com/edusn/Digizenger/Demo/dashboard/service/AdminDashBoardService.java
+++ src/main/java/com/edusn/Digizenger/Demo/dashboard/service/AdminDashBoardService.java
@@ -2,6 +2,7 @@ package com.edusn.Digizenger.Demo.dashboard.service;
 import com.edusn.Digizenger.Demo.auth.dto.response.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import java.util.Map;
 
 public interface AdminDashBoardService  {
 
@@ -12,5 +13,7 @@ public interface AdminDashBoardService  {
     ResponseEntity<Response> getNewUserIn30Days(int page, int limit);
 
     ResponseEntity<Response> getVerifiedUsers(int page, int limit);
+
+    Map<String ,Object>  getSuspendedUsers (int page , int limit , int days);
 
 }
